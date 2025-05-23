540bb221409cf131fa79a308592c8e1151df0d60
Deepti Mahisare <115733808+deeptimahisare1206@users.noreply.github.com>
2025-05-23 14:42:42 +0530

    Add files via upload

--- join_table/src/main/java/com/mycode/controller/CoordinatorControl.java
+++ join_table/src/main/java/com/mycode/controller/CoordinatorControl.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.mycode.models.Coordinator;
+import com.mycode.models.RegistrationDTO;
 import com.mycode.services.CoordinatorServices;
 
 @RestController
@@ -23,10 +24,12 @@ public class CoordinatorControl {
 
   
 	@PostMapping("/register")
-	public ResponseEntity<Coordinator> newcoordinator(@RequestBody Coordinator request){
-//		return  ResponseEntity.ok("Coordinator registerd successfully!");
+	public ResponseEntity<String> newcoordinator(@RequestBody RegistrationDTO request){
 		coordinatorServices.newcoordinator(request);
-		return new ResponseEntity<Coordinator>(HttpStatus.OK);
+		return ResponseEntity.status(HttpStatus.CREATED).body("Coordinator registered successfully!");
+
+//		return new ResponseEntity<Coordinator>(HttpStatus.OK);
+//		return  ResponseEntity.ok("Coordinator registerd successfully!");
 	}
 	@GetMapping("/allcoordinators")
 	public ResponseEntity<List<Coordinator>> allCoordinatorsList(){
