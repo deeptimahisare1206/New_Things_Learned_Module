7d5d15f9ced797b86803927c438f3fdcdb895067
Deepti Mahisare <115733808+deeptimahisare1206@users.noreply.github.com>
2025-11-26 22:07:54 +0530

    Update JsonFileStoringApplicationTests.java

--- json_file_Storing/src/test/java/com/mycode/JsonFileStoringApplicationTests.java
+++ json_file_Storing/src/test/java/com/mycode/JsonFileStoringApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JsonFileStoringApplicationTests {
-
+//write testing code here.
 	@Test
 	void contextLoads() {
 	}
