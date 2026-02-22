f1996c26e67220f78fa7796c5c122e73a2360a32
Deepti Mahisare <115733808+deeptimahisare1206@users.noreply.github.com>
2026-02-22 22:33:23 +0530

    Add comment for test case implementation
    Added a comment to indicate where to write test cases.

--- join_table/src/test/java/com/mycode/JoinTableApplicationTests.java
+++ join_table/src/test/java/com/mycode/JoinTableApplicationTests.java
@@ -5,7 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JoinTableApplicationTests {
-
+//Write the test cases here 
+	
 	@Test
 	void contextLoads() {
 	}
