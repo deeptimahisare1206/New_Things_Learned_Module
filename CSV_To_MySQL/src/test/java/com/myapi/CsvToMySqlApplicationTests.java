fffd6a6f88c500f3ef51928de159055a833723bc
Deepti Mahisare <115733808+deeptimahisare1206@users.noreply.github.com>
2026-03-12 22:25:49 +0530

    Add comment for test case location
    Added a comment indicating where to write test cases.

--- CSV_To_MySQL/src/test/java/com/myapi/CsvToMySqlApplicationTests.java
+++ CSV_To_MySQL/src/test/java/com/myapi/CsvToMySqlApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CsvToMySqlApplicationTests {
-
+//Write the test cases here..
 	@Test
 	void contextLoads() {
 	}
